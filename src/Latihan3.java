f7eee85fcf70a7cbc7f45dd26afb615e71c51bed
Fannizaaisya <fannizaaisya@gmail.com>
2016-03-18 23:32:12 +0700

    155150201111321_FannizaAisya

---
+++ src/Latihan3.java
@@ -0,0 +1,14 @@
+package latihan3;
+public class Latihan3 {
+    private int luas1;
+    private int luas2;
+    public int getluas1 (int al1, int al2, int ti){
+        luas1 = (al1+al2)*ti/2;
+        return luas1;
+    }
+    public int getluas2 (int pa, int le){
+        luas2 = pa*le;
+        return luas2;
+    }
+    
+}
