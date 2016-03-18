cfc3f3a3f7518f343efdd96942a8b34fa973ce51
ziodark <jio.fr65@gmail.com>
2016-03-18 23:33:47 +0700

    155150201111322_FauziRezaPahlevi

---
+++ src/bangun.java
@@ -0,0 +1,20 @@
+package Bangun;
+
+/**
+ *
+ * @author USER
+ */
+public class bangun {
+    private double PP;
+    private double Trp;
+    public bangun(){
+        PP = 0;
+        Trp = 0;
+    }
+    public double getLuas(double p, double l){
+        return PP = p*l;
+    }
+    public double getLuas(double a, double b, double t){
+        return Trp = ((a+b)*t)/2;
+    }
+}
