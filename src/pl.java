de46d6a130c30624ba4ab0c679d298b778d76b57
I Gede Adi Brahman Nugraha <adiforever99@gmail.com>
2016-03-21 00:53:08 +0700

    155150201111334_I GEDE ADI BRAHMAN NUGRAHA
    maaf saya terlambat mengumpulkan karena kepanitiaan acara ub.

--- src/pl.java
+++ src/pl.java
@@ -1,4 +1,18 @@
 
 public class pl {
-
+    double luas,total;
+    public void Luas(double p,double l){
+        luas = p*l;
+        total+=luas;
+    }
+    public void Luas(double a,double b,double t){
+        luas = ((a+b)*t)/2;
+        total+=luas;
+    }
+    public double getLuas(){
+        return luas;
+    }
+    public double jumlahkeduabidang(){
+        return total;
+    }
 }
